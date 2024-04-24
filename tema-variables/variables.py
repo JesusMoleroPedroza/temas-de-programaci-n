c6ae92fb5b2576c41c74f701e4e589e00cb0ca47
JesusMoleroPedroza <160894545+JesusMoleroPedroza@users.noreply.github.com>
2024-04-24 09:31:04 -0500

    Add files via upload

---
+++ tema-variables/variables.py
@@ -0,0 +1,60 @@
+#VARIABLES
+
+#En Python, una variable es un nombre que se utiliza para hacer referencia a un valor almacenado en la memoria.
+#Las variables se pueden crear simplemente asignando un valor a un nombre utilizando el operador de asignación "=".
+#No es necesario declarar explícitamente el tipo de una variable en Python; el tipo se infiere automáticamente según el valor asignado.
+#Los nombres de las variables en Python pueden contener letras, números y guiones bajos, pero deben comenzar con una letra o un guion bajo.
+
+#ejemplo
+edad = 25
+nombre = "Juan"
+
+#Tipos de datos:
+#Python tiene varios tipos de datos incorporados, incluyendo:
+#Números: enteros (int), flotantes (float) y complejos (complex).
+#Cadenas de caracteres: se pueden definir utilizando comillas simples ('') o dobles ("").
+#Booleanos: True o False.
+#Listas: colecciones ordenadas y mutables de elementos.
+#Tuplas: colecciones ordenadas e inmutables de elementos.
+#Diccionarios: colecciones no ordenadas de pares clave-valor.
+
+#ejemplo:
+entero = 10
+flotante = 3.14
+cadena = "Hola, mundo!"
+booleano = True
+lista = [1, 2, 3]
+tupla = (4, 5, 6)
+diccionario = {"nombre": "Juan", "edad": 25}
+
+#Operadores:
+#Python admite varios tipos de operadores, incluyendo operadores aritméticos, de comparación, de asignación, lógicos y de identidad.
+#Operadores aritméticos: se utilizan para realizar operaciones matemáticas básicas, como suma (+), resta (-), multiplicación (*), división (/), módulo (%) y potenciación (**).
+#Operadores de comparación: se utilizan para comparar valores, como igualdad (==), desigualdad (!=), mayor que (>), menor que (<), etc.
+#Operadores de asignación: se utilizan para asignar valores a variables, como el operador de asignación simple (=) o los operadores compuestos (+=, -=, *=, /=, etc.).
+#Operadores lógicos: se utilizan para combinar expresiones booleanas, como AND (and), OR (or) y NOT (not).
+#Operadores de identidad: se utilizan para comprobar si dos variables se refieren al mismo objeto en memoria, como is y is not.
+#Operadores de pertenencia: se utilizan para comprobar si un valor está presente en una secuencia, como in y not in.
+
+#ejemplo:
+# Operadores aritméticos
+suma = 10 + 5
+resta = 10 - 5
+multiplicacion = 10 * 5
+division = 10 / 5
+modulo = 10 % 5
+potenciacion = 10 ** 2
+
+# Operadores de comparación
+igualdad = 10 == 5
+desigualdad = 10 != 5
+mayor_que = 10 > 5
+menor_que = 10 < 5
+
+# Operadores lógicos
+y_logico = True and False
+o_logico = True or False
+negacion = not True
+
+# Operadores de identidad
+identidad = (10 is 10)
